5adebdee3a6636af9e53d40fb068da61b9472cbb
Juan Pablo Sánchez Rey <juanpablosanchez744@gmail.com>
2021-11-04 09:12:04 -0500

    version 3.0

--- mail/contact.php
+++ mail/contact.php
@@ -1,13 +1,16 @@
+
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
-}
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
+
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
 
 $to = "juanpablosanchez744@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
