a4e71911104a216c1021b86fc9ecb721f8131922
Juan Pablo Sánchez Rey <juanpablosanchez744@gmail.com>
2021-11-04 11:57:20 -0500

    version 3.92

--- mail/enviar.php
+++ mail/enviar.php
@@ -5,12 +5,12 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$to = "juanpablosanchez744@gmail.com"; // Change this email to your //
+// Change this email to your //
 $subject = "contacto desde la pagina web";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
-$mail($to, $subject, $body, $header);
+mail($destino, $subject, $body, $header);
 header('Location:index.html');
 ?>
